bc8f30a94aa6c39e27dba3930ac7ecbd4d0f7a1c
Phil Spitler (PRS) <prs@sproutkey.com>
2015-01-15 16:48:27 -0500

    implemented proxy for review apis

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,26 +7,26 @@ app.controller('SearchController', function SearchController($scope, NYTimes, $h
     //console.log($http.jsonp('http://api.nytimes.com/svc/movies/v2/reviews/search.json?query=gladiator&api-key=ddb559a750ed89a31dc492e01db33c1f:5:70698674'));
     NYTimes.keywordSearch($scope.query, function(error, data) {
         if (!error) {
-          $scope.movies = callback.results;
+          $scope.movies = data;
         }
     });
-  }
+  };
 });
 
 
 app.factory('NYTimes', ['$http', function($http) {
     return {
         keywordSearch: function(query, callback) {
-        var requestURI = 'http://api.nytimes.com/svc/movies/v2/reviews/search.json?query=' + query + '&api-key=ddb559a750ed89a31dc492e01db33c1f:5:70698674';
-        $http.jsonp(requestURI)
+          var requestURI = '/api';
+        $http.post(requestURI, {query: query})
           .success(function (data) {
               callback(null, data);
-              console.log(data);
           })
           .error(function (e) {
               callback(e);
           });
       }
+    };
 }]);
 
 
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+var Searcher = require('./lib/searcher');
+var ProviderFactory = require('./lib/providers');
 var Hapi = require('hapi'),
     path = require('path'),
+    request = require('request'),
     port = process.env.PORT || 3000,
     server = new Hapi.Server(port),
     routes = {
@@ -23,28 +26,76 @@ var Hapi = require('hapi'),
             path: '/templates/{path*}',
             handler: createDirectoryRoute('templates')
         },
+        //nytimes: {
+        //  method: 'GET',
+        //  path: '/nytimes',
+        //  handler: {
+        //    proxy: {
+        //      mapUri: function (request, callback) {
+        //        var url = 'http://api.nytimes.com/svc/movies/v2/reviews/search.json?query=' + request.query.query + '&api-key=ddb559a750ed89a31dc492e01db33c1f:5:70698674';
+        //        callback(null,url);
+        //      },
+        //      //onResponse: function(err, res, request, reply, settings, ttl) {
+
+        //      //},
+        //      passThrough: true,
+        //      redirects: 5
+        //    }
+        //  }
+        //},
+        nytimes: {
+          method: 'POST',
+          path: '/api',
+          handler: getFeeds
+        },
         spa: {
-            method: 'GET',
-            path: '/{path*}',
-            handler: {
-                file: path.join(__dirname, '/dist/index.html')
-            }
+          method: 'GET',
+          path: '/{path*}',
+          handler: {
+            file: path.join(__dirname, '/dist/index.html')
+          }
         }
     };
 
-server.route([ routes.css, routes.js, routes.images, routes.templates, routes.spa ]);
-server.start( onServerStarted );
+    server.route([ routes.css, routes.js, routes.images, routes.templates, routes.nytimes, routes.spa ]);
+    server.start( onServerStarted );
 
-function onServerStarted() {
-    console.log( 'Server running on port ', port );
-}
+    function getFeeds(req, reply) {
+      var searcher = new Searcher();
 
-function createDirectoryRoute( directory ) {
-    return {
+      searcher.add(ProviderFactory.build('nytimes', {
+        hostname: 'api.nytimes.com',
+        basepath: '/svc/movies/v2/reviews/search.json',
+        apiKey: 'ddb559a750ed89a31dc492e01db33c1f:5:70698674'
+      }));
+
+      searcher.add(ProviderFactory.build('usatoday', {
+        hostname: 'api.usatoday.com',
+        basepath: '/open/reviews/movies/movies',
+        apiKey: '7p665vjpvsn2drnk6w347x5w'
+      }));
+
+      searcher.search(req.payload.query).then(
+        function pass(items) {
+        reply(items);
+      },
+      function fail(err) {
+        console.log(err);
+      });
+    }
+
+
+
+    function onServerStarted() {
+      console.log( 'Server running on port ', port );
+    }
+
+    function createDirectoryRoute( directory ) {
+      return {
         directory: {
-            path: path.join(__dirname, '/dist/', directory)
+          path: path.join(__dirname, '/dist/', directory)
         }
-    };
-}
+      };
+    }
 
-module.exports = server;
+    module.exports = server;
